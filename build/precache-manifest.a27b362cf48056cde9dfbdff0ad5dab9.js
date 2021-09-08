f0b2b2060a53f76357c9f808aea54bbae147ca8b
Uzma Nareen <nareen.khan98@gmail.com>
2021-09-08 12:48:25 +0530

    first commit

---
+++ build/precache-manifest.a27b362cf48056cde9dfbdff0ad5dab9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a630ba6a36386435de35e4723cd3e062",
+    "url": "/ReactWeather/index.html"
+  },
+  {
+    "revision": "21aa932fbf53cf7f776f",
+    "url": "/ReactWeather/static/css/2.00ab6c24.chunk.css"
+  },
+  {
+    "revision": "e651764a2d7a952e09d7",
+    "url": "/ReactWeather/static/css/main.365c8841.chunk.css"
+  },
+  {
+    "revision": "21aa932fbf53cf7f776f",
+    "url": "/ReactWeather/static/js/2.3c097907.chunk.js"
+  },
+  {
+    "revision": "e651764a2d7a952e09d7",
+    "url": "/ReactWeather/static/js/main.067e1cb8.chunk.js"
+  },
+  {
+    "revision": "8695f24f3e6bd75126ff",
+    "url": "/ReactWeather/static/js/runtime~main.367061c1.js"
+  },
+  {
+    "revision": "18f95f62829e6475297b1f21b3c5baf2",
+    "url": "/ReactWeather/static/media/w.18f95f62.png"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/ReactWeather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/ReactWeather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/ReactWeather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/ReactWeather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/ReactWeather/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
